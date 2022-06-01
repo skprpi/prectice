a8cb57665f9ecee0d31fee57f2dc82c64057ee2c
Maxim <fatin.maksim9067@gmail.com>
2022-06-01 21:40:56 +0300

    fix: Fix return type

--- f/task21.fs
+++ f/task21.fs
@@ -2,19 +2,20 @@ open System
 
 // 50.2.1
 let fac_seq =
-    let rec helper i res n =
-        match i with
-            | x when x = n -> seq {yield res}
-            | x -> seq { yield res; yield! (helper (x + 1) (res * (x + 1)) n ) }
-    helper 0 1
+    seq {
+        let mutable i = 0
+        let mutable result = 1
+        while true do
+            result <- result * (if i = 0 then 1 else i)
+            i <- i + 1
+            yield result
+    }
 
 // 50.2.2
 let seq_seq =
-    let rec helper i n =
-        match i with
-            | x when x = n -> seq {yield ((x + 1) / 2 * -1 * (if x % 2 = 0 then -1 else 1))}
-            | x -> seq {
-                yield ((x + 1) / 2 * -1 * (if x % 2 = 0 then -1 else 1))
-                yield! (helper (x + 1) n) 
-            }
-    helper 0
+    seq {
+        let mutable x = 0
+        while true do
+            yield ((x + 1) / 2 * -1 * (if x % 2 = 0 then -1 else 1))
+            x <- x + 1
+    }
